0a1377ecbd7f67fb750b85b81ddb5fbdce42b2eb
tvatt <github@vattima.com>
2025-03-02 08:37:12 -0500

    Fixes profiles and yml files

--- src/main/java/net/legohunter/sandbox/BricklinkOrderJobConfiguration.java
+++ src/main/java/net/legohunter/sandbox/BricklinkOrderJobConfiguration.java
@@ -1,8 +1,10 @@
 package net.legohunter.sandbox;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Configuration
 @Profile("bricklink-order-job")
 public class BricklinkOrderJobConfiguration {
     @Bean
